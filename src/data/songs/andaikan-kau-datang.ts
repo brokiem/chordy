7c9b50e24f630175f0aa17d07dd0c3a5c125f329
brokiem <milyhqvideo@gmail.com>
2025-12-27 19:21:08 +0800

    feat: split songs into their own file for easier management

---
+++ src/data/songs/andaikan-kau-datang.ts
@@ -0,0 +1,303 @@
+import type { Song } from '../../types';
+
+export const andaikanKauDatang: Song = {
+  id: 'andaikan-kau-datang',
+  title: 'Andaikan Kau Datang',
+  artist: 'Koes Plus',
+  key: 'G',
+  lines: [
+    {
+      lyrics: '[Intro]',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 5 },
+        { name: 'G', position: 10 },
+        { name: 'Am', position: 15 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'Terlalu indah dilupakan',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 14 },
+      ],
+    },
+    {
+      lyrics: 'Terlalu sedih dikenangkan',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 14 },
+      ],
+    },
+    {
+      lyrics: 'Setelah aku jauh berjalan',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 18 },
+      ],
+    },
+    {
+      lyrics: 'Dan kau kutinggalkan',
+      chords: [
+        { name: 'D', position: 4 },
+        { name: 'G', position: 13 },
+        { name: 'C', position: 17 },
+        { name: 'D', position: 19 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'Betapa hatiku bersedih',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 14 },
+      ],
+    },
+    {
+      lyrics: 'Mengenang kasih dan sayangmu',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Setulus pesanmu kepadaku',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 18 },
+      ],
+    },
+    {
+      lyrics: 'Engkau kan menunggu',
+      chords: [
+        { name: 'D', position: 3 },
+        { name: 'G', position: 13 },
+        { name: 'C', position: 17 },
+        { name: 'D', position: 19 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Chorus]',
+      chords: [],
+    },
+    {
+      lyrics: 'Andaikan kau datang kemari',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Jawaban apa yang kan kuberi',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 21 },
+      ],
+    },
+    {
+      lyrics: 'Adakah jalan yang kau temui',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 21 },
+      ],
+    },
+    {
+      lyrics: 'Untuk kita kembali lagi',
+      chords: [
+        { name: 'D', position: 2 },
+        { name: 'G', position: 15 },
+        { name: 'C', position: 19 },
+        { name: 'D', position: 21 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'Bersinarlah bulan purnama',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Seindah serta tulus cintanya',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 22 },
+      ],
+    },
+    {
+      lyrics: 'Bersinarlah terus sampai nanti',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 24 },
+      ],
+    },
+    {
+      lyrics: 'Lagu ini kuakhiri',
+      chords: [
+        { name: 'D', position: 2 },
+        { name: 'G', position: 11 },
+        { name: 'C', position: 15 },
+        { name: 'D', position: 17 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'Betapa hatiku bersedih',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 14 },
+      ],
+    },
+    {
+      lyrics: 'Mengenang kasih dan sayangmu',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Setulus pesanmu kepadaku',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 18 },
+      ],
+    },
+    {
+      lyrics: 'Engkau kan menunggu',
+      chords: [
+        { name: 'D', position: 3 },
+        { name: 'G', position: 13 },
+        { name: 'C', position: 17 },
+        { name: 'D', position: 19 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Chorus]',
+      chords: [],
+    },
+    {
+      lyrics: 'Andaikan kau datang kemari',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Jawaban apa yang kan kuberi',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 21 },
+      ],
+    },
+    {
+      lyrics: 'Adakah jalan yang kau temui',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 21 },
+      ],
+    },
+    {
+      lyrics: 'Untuk kita kembali lagi',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 17 },
+        { name: 'C', position: 21 },
+        { name: 'D', position: 23 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'Bersinarlah bulan purnama',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Seindah serta tulus cintanya',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 22 },
+      ],
+    },
+    {
+      lyrics: 'Bersinarlah terus sampai nanti',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 24 },
+      ],
+    },
+    {
+      lyrics: 'Lagu ini kuakhiri',
+      chords: [
+        { name: 'D', position: 2 },
+        { name: 'G', position: 11 },
+        { name: 'C', position: 15 },
+        { name: 'D', position: 17 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Ending]',
+      chords: [],
+    },
+    {
+      lyrics: 'Bersinarlah bulan purnama',
+      chords: [
+        { name: 'G', position: 0 },
+        { name: 'Am', position: 20 },
+      ],
+    },
+    {
+      lyrics: 'Seindah serta tulus cintanya',
+      chords: [
+        { name: 'D', position: 0 },
+        { name: 'G', position: 22 },
+      ],
+    },
+    {
+      lyrics: 'Bersinarlah terus sampai nanti',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'G', position: 24 },
+      ],
+    },
+    {
+      lyrics: 'Lagu ini kuakhiri',
+      chords: [
+        { name: 'D', position: 2 },
+        { name: 'G', position: 11 },
+        { name: 'C', position: 15 },
+        { name: 'D', position: 17 },
+      ],
+    },
+  ],
+};
---
+++ src/data/songs/iris.ts
@@ -0,0 +1,358 @@
+import type { Song } from '../../types';
+
+export const iris: Song = {
+  id: 'iris',
+  title: 'Iris',
+  artist: 'Goo Goo Dolls',
+  key: 'G',
+  lines: [
+    {
+      lyrics: '[Intro]',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'C', position: 10 },
+        { name: 'Em', position: 20 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: "And I'd give up forever to touch you",
+      chords: [
+        { name: 'G', position: 8 },
+        { name: 'Am', position: 16 },
+        { name: 'C', position: 27 },
+      ],
+    },
+    {
+      lyrics: 'Cause I know that you feel me somehow',
+      chords: [
+        { name: 'Em', position: 8 },
+        { name: 'D', position: 22 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: "You're the closest to heaven",
+      chords: [
+        { name: 'G', position: 11 },
+        { name: 'Am', position: 22 },
+      ],
+    },
+    {
+      lyrics: "that I'll ever be",
+      chords: [{ name: 'C', position: 5 }],
+    },
+    {
+      lyrics: "And I don't want to go home right now",
+      chords: [
+        { name: 'Em', position: 6 },
+        { name: 'D', position: 20 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'And all I can taste is this moment',
+      chords: [
+        { name: 'G', position: 4 },
+        { name: 'Am', position: 14 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: 'And all I can breathe is your life',
+      chords: [
+        { name: 'Em', position: 4 },
+        { name: 'D', position: 14 },
+        { name: 'C', position: 29 },
+      ],
+    },
+    {
+      lyrics: "Cause sooner or later it's over",
+      chords: [
+        { name: 'G', position: 6 },
+        { name: 'Am', position: 16 },
+        { name: 'C', position: 27 },
+      ],
+    },
+    {
+      lyrics: "I just don't want to miss you tonight",
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 21 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Chorus]',
+      chords: [],
+    },
+    {
+      lyrics: "And I don't want the world to see me",
+      chords: [
+        { name: 'Em', position: 6 },
+        { name: 'D', position: 21 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: "Cause I don't think that they'd understand",
+      chords: [
+        { name: 'Em', position: 8 },
+        { name: 'D', position: 25 },
+        { name: 'C', position: 32 },
+      ],
+    },
+    {
+      lyrics: "When everything's made to be broken",
+      chords: [
+        { name: 'Em', position: 5 },
+        { name: 'D', position: 18 },
+        { name: 'C', position: 29 },
+      ],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Intro]',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'C', position: 10 },
+        { name: 'Em', position: 20 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: "And you can't fight the tears that ain't coming",
+      chords: [
+        { name: 'G', position: 8 },
+        { name: 'Am', position: 24 },
+        { name: 'C', position: 41 },
+      ],
+    },
+    {
+      lyrics: 'Or the moment of truth in your lies',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 18 },
+        { name: 'C', position: 31 },
+      ],
+    },
+    {
+      lyrics: 'When everything feels like the movies',
+      chords: [
+        { name: 'G', position: 5 },
+        { name: 'Am', position: 20 },
+        { name: 'C', position: 31 },
+      ],
+    },
+    {
+      lyrics: 'Yeah you bleed just to know your alive',
+      chords: [
+        { name: 'Em', position: 9 },
+        { name: 'D', position: 23 },
+        { name: 'C', position: 33 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Chorus]',
+      chords: [],
+    },
+    {
+      lyrics: "And I don't want the world to see me",
+      chords: [
+        { name: 'Em', position: 6 },
+        { name: 'D', position: 21 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: "Cause I don't think that they'd understand",
+      chords: [
+        { name: 'Em', position: 8 },
+        { name: 'D', position: 25 },
+        { name: 'C', position: 32 },
+      ],
+    },
+    {
+      lyrics: "When everything's made to be broken",
+      chords: [
+        { name: 'Em', position: 5 },
+        { name: 'D', position: 18 },
+        { name: 'C', position: 29 },
+      ],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Interlude]',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'C', position: 10 },
+        { name: 'Em', position: 20 },
+        { name: 'C', position: 30 },
+        { name: 'Em', position: 40 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'Bm', position: 5 },
+        { name: 'C', position: 10 },
+        { name: 'Em', position: 15 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [
+        { name: 'C', position: 0 },
+        { name: 'Bm', position: 5 },
+        { name: 'Em', position: 10 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'D', position: 6 },
+        { name: 'G', position: 12 },
+        { name: 'Em', position: 18 },
+        { name: 'D', position: 24 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'D', position: 6 },
+        { name: 'G', position: 12 },
+        { name: 'Em', position: 18 },
+        { name: 'D', position: 24 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Chorus]',
+      chords: [],
+    },
+    {
+      lyrics: "And I don't want the world to see me",
+      chords: [
+        { name: 'Em', position: 6 },
+        { name: 'D', position: 21 },
+        { name: 'C', position: 30 },
+      ],
+    },
+    {
+      lyrics: "Cause I don't think that they'd understand",
+      chords: [
+        { name: 'Em', position: 8 },
+        { name: 'D', position: 25 },
+        { name: 'C', position: 32 },
+      ],
+    },
+    {
+      lyrics: "When everything's made to be broken",
+      chords: [
+        { name: 'Em', position: 5 },
+        { name: 'D', position: 18 },
+        { name: 'C', position: 29 },
+      ],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'C', position: 28 },
+      ],
+    },
+    {
+      lyrics: 'I just want you to know who I am',
+      chords: [
+        { name: 'Em', position: 7 },
+        { name: 'D', position: 19 },
+        { name: 'Em', position: 28 },
+        { name: 'D', position: 31 },
+        { name: 'G', position: 33 },
+      ],
+    },
+    {
+      lyrics: '',
+      chords: [],
+    },
+    {
+      lyrics: '[Outro]',
+      chords: [
+        { name: 'Em', position: 0 },
+        { name: 'D', position: 10 },
+        { name: 'C', position: 20 },
+      ],
+    },
+  ],
+};
